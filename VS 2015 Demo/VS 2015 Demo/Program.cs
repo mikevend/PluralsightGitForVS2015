2b65f2be792f07bc76cd4fb516e0d56dc8da8c73
Michael Venditti <mike.vend@outlook.com>
2017-05-12 09:55:55 -0400

    Initial VS 2015 Commit

---
+++ VS 2015 Demo/VS 2015 Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VS_2015_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
